241df3679f113e6cc14882c6d201c18ae79e2dd6
João Hudson <joao.hudson@taking.com.br>
2025-04-09 15:21:54 -0300

    feat: add validation and new input

--- src/main/java/br/com/anagram/generator/Main.java
+++ src/main/java/br/com/anagram/generator/Main.java
@@ -2,14 +2,20 @@ package br.com.anagram.generator;
 
 import br.com.anagram.generator.utils.AnagramUtils;
 
+import java.util.Scanner;
 import java.util.Set;
+
+import static br.com.anagram.generator.constants.PatternConstantes.ONLY_LETTERS;
 
 public class Main {
 	public static void main(String[] args) {
-		if(args.length != 1) {
-			throw new IllegalArgumentException("Single text parameter required!");
+		System.out.println("Enter a text containing letters only to generate the permutations:");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		if(!ONLY_LETTERS.matcher(input).matches()) {
+			System.err.println("You must provide a non-empty text containing letters only.");
+			return;
 		}
-		String input = args[0];
 		Set<String> result = AnagramUtils.generate(input);
 		String resultDisplayText = String.join("\n", result);
 		System.out.println(resultDisplayText);
---
+++ src/main/java/br/com/anagram/generator/constants/PatternConstantes.java
@@ -0,0 +1,7 @@
+package br.com.anagram.generator.constants;
+
+import java.util.regex.Pattern;
+
+public class PatternConstantes {
+	public static final Pattern ONLY_LETTERS = Pattern.compile("[a-zA-Z]+");
+}
